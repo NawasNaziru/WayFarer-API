21cd570612aff90981650ba2cd9751c09a32c672
Nawas Naziru Adam <nawasnaziru@gmail.com>
2019-06-28 19:17:08 +0100

    chore(express,eslint,mocha):install and setup express, eslint and mocha
    [Finishes #166976567]

---
+++ server/app.js
@@ -0,0 +1,75 @@
+import dotenv from from 'dotenv';
+dotenv.load();
+import express from 'express';
+import path from 'path';
+import logger from 'morgan';
+import cookieParser from 'cookie-parser';
+import bodyParser from 'body-parser';
+import Debug from 'debug';
+const debug = Debug('Express4');
+
+import  router from './routes/index';
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use(express.static(path.join(__dirname, 'UI')));
+
+app.use('/api/v1', router);
+
+app.use((req, res) => {
+  res.sendFile(path.join(__dirname, 'UI', 'index.html'));
+});
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+// Catch unauthorised errors
+app.use((err, req, res) => {
+  if (err.name === 'UnauthorizedError') {
+    res.status(401);
+    res.json({ message: `${err.name}: ${err.message}` });
+  }
+});
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use((err, req, res) => {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err,
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use((err, req, res) => {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {},
+  });
+});
+
+// start server
+
+app.set('port', process.env.PORT || 3000);
+
+const server = app.listen(app.get('port'), () => {
+  debug('Express server listening on port ' + server.address().port);
+ console.log('Express server listening on localhost:3000');
+});
+
+export default app;
