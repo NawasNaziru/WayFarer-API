1d758f82fa092ed7ac1a4ea8d739eafaf7e3c5e4
Nawas Naziru Adam <nawasnaziru@gmail.com>
2019-06-29 07:41:53 +0100

    bug(express):adds routes file and skeleton controllers codes
    [Delivers #166976567]

---
+++ server/controllers/trips.js
@@ -0,0 +1,16 @@
+const sendJSONresponse = (res, status, content) => {
+  res.status(status);
+  res.json(content);
+};
+
+
+export const createTrip =  (req, res) => {
+
+};
+
+export const viewAllTrips = (req, res) => {
+
+};
+
+const Num_of_handlers = 3;
+export default Num_of_handlers;
