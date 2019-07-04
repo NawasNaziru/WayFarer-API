8bc1fd47ca0fda58c4819936b6200309b5fa5fcc
Nawas Naziru Adam <nawasnaziru@gmail.com>
2019-07-04 17:52:57 +0100

    feature(sign-up):add user signup controller module function
    [Finishes #166954007]

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -27,9 +27,80 @@ const config = {
 	
 const pool = new pg.Pool(config);
  
+
 export const signup = (req, res) => {
 
-};
+  if (!req.body.first_name || !req.body.last_name || !req.body.email || !req.body.password) {
+    sendJSONresponse(res, 400, {
+      status: 'error',
+      error: 'Requires your first name, last name, email and password!',
+    });
+    return;
+  }
+
+  const generateJwt = id => {
+    const expiry = new Date();
+    expiry.setDate(expiry.getDate() + 7);
+  
+    return jwt.sign({
+      _id: id,
+      email: req.body.email,
+      name: req.body.firstName,
+      exp: parseInt((expiry.getTime() / 1000), 10),
+    }, process.env.JWT_SECRET); // DO NOT KEEP YOUR SECRET IN THE CODE!
+  };
+  
+  const hash = bcrypt.hashSync(req.body.password, salt);
+
+  const firstName = req.body.first_name;
+  const lastName = req.body.last_name;
+  const email = req.body.email;
+  const isAdmin = req.body.is_admin === 'true' ? true: false; //convert string to boolean.
+
+  const query = {
+    text: 'INSERT INTO users(is_admin, email, first_name, last_name, salt, hash) VALUES( $1, $2, $3, $4, $5, $6) RETURNING *',
+    values: [isAdmin, email, firstName, lastName, salt, hash]
+  }
+
+  pool.connect((err,client,done) => {
+    if(err){
+      sendJSONresponse(res, 500, {
+        status: 'error',
+        error: 'Could not connect to database'
+      })
+        return;
+    } 
+    client.query(query).then(responseData => {
+      if(responseData.rows.length === 0){
+        sendJSONresponse(res,404, {
+          status: 'error',
+          error: 'Unsuccessful. Try again!'
+        } );
+        return;
+      }
+      const userId = responseData.rows[0].user_id;
+      const token = generateJwt(userId);
+      sendJSONresponse(res, 201, {
+      status: 'success',
+      data: {
+        user_id: responseData.rows[0].user_id,
+        is_admin: responseData.rows[0].is_admin,
+        token: token,
+        email: responseData.rows[0].email,
+        first_name: responseData.rows[0].first_name,
+        last_name: responseData.rows[0].last_name,
+      }
+    })
+  }
+    ).catch(e => sendJSONresponse(res, 500, {
+      status: 'error',
+      error: 'Already signed up. Sign in instead'
+    })
+    );
+
+ });
+}
+
 
 export const signin = (req, res) => {
 
