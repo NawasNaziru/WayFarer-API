4d69326f90fa4c210226a7ecb492f253f5afc092
Nawas Naziru Adam <nawasnaziru@gmail.com>
2019-07-06 10:58:37 +0100

    chore(token): extend token expiry to 27 days
    [Finishes 166954007]

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -40,7 +40,7 @@ export const signup = (req, res) => {
 
   const generateJwt = id => {
     const expiry = new Date();
-    expiry.setDate(expiry.getDate() + 7);
+    expiry.setDate(expiry.getDate() + 27);
   
     return jwt.sign({
       _id: id,
