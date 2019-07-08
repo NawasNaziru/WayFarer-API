06df7bae21ec65718025ed0efe0ddaf948b04671
NawasNaziru <nawasnaziru@gmail.com>
2019-07-08 06:32:48 +0100

    chore(travis):change port to the default 5432

--- server/controllers/trips.js
+++ server/controllers/trips.js
@@ -12,7 +12,7 @@ const config = {
   user: 'postgres',
   database: 'wayfarer_api_db', 
   password: '', 
-  port: PGPORT, 
+  port: 5432, 
   max: 10, // max number of connection can be open to database
   idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
 };
