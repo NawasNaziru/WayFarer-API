26b230d33274696f263674e5022ad67956be40a0
Nawas Naziru Adam <nawasnaziru@gmail.com>
2019-07-30 17:02:50 +0100

    bg:fix syntax error in wayfarer-db.sql file
    [Delivers #167534550]

--- server/models/wayfarer-db.sql
+++ server/models/wayfarer-db.sql
@@ -1,7 +1,7 @@
 BEGIN;
 CREATE TABLE users(user_id SERIAL PRIMARY KEY, is_admin BOOLEAN not null DEFAULT false, email TEXT not null UNIQUE, first_name TEXT not null, last_name TEXT not null, salt TEXT not null UNIQUE, hash TEXT not null UNIQUE);
 CREATE TABLE buses(bus_id SERIAL PRIMARY KEY, number_plate TEXT not null UNIQUE, manufacturer TEXT not null, model  TEXT not null, year VARCHAR(4) not null, capacity INT not null);
-CREATE TABLE trips(id PRIMARY KEY, bus_id INTEGER REFERENCES buses(bus_id) , origin TEXT not null, destination  TEXT not null, trip_date TIMESTAMP not null, fare REAL not null, status TEXT DEFAULT 'active');
+CREATE TABLE trips(id SERIAL PRIMARY KEY, bus_id INTEGER REFERENCES buses(bus_id) , origin TEXT not null, destination  TEXT not null, trip_date TIMESTAMP not null, fare REAL not null, status TEXT DEFAULT 'active');
 CREATE TABLE bookings(id SERIAL, trip_id INT REFERENCES trips(id), user_id INT REFERENCES users(user_id), seat_number SERIAL UNIQUE, created_on  TIMESTAMP not null, PRIMARY KEY (trip_id, user_id));
 CREATE SEQUENCE trips_sequence START 1 INCREMENT 1;
 INSERT INTO users(is_admin, email, first_name, last_name, salt, hash ) VALUES (true, 'zabeed@outlook.com', 'zinel', 'abeed', '$2b$10$E2b5fl4RcZy0zg2WHxHLbO', '$2b$10$E2b5fl4RcZy0zg2WHxHLbOVU.2GaO9KXzxbvHCH7dgiBXLR.YbF7m');
