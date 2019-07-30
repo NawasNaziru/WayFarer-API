2cee52716ca375c9a6a1c6fc9d511a442cc8b648
Nawas Naziru Adam <nawasnaziru@gmail.com>
2019-07-30 17:22:51 +0100

    bg(postgresql):fix syntax error and use generic sequence
    [Delivers #167534550]

--- server/models/wayfarer-db.sql
+++ server/models/wayfarer-db.sql
@@ -3,9 +3,9 @@ CREATE TABLE users(user_id SERIAL PRIMARY KEY, is_admin BOOLEAN not null DEFAULT
 CREATE TABLE buses(bus_id SERIAL PRIMARY KEY, number_plate TEXT not null UNIQUE, manufacturer TEXT not null, model  TEXT not null, year VARCHAR(4) not null, capacity INT not null);
 CREATE TABLE trips(id SERIAL PRIMARY KEY, bus_id INTEGER REFERENCES buses(bus_id) , origin TEXT not null, destination  TEXT not null, trip_date TIMESTAMP not null, fare REAL not null, status TEXT DEFAULT 'active');
 CREATE TABLE bookings(id SERIAL, trip_id INT REFERENCES trips(id), user_id INT REFERENCES users(user_id), seat_number SERIAL UNIQUE, created_on  TIMESTAMP not null, PRIMARY KEY (trip_id, user_id));
-CREATE SEQUENCE trips_sequence START 1 INCREMENT 1;
-INSERT INTO users(is_admin, email, first_name, last_name, salt, hash ) VALUES (true, 'zabeed@outlook.com', 'zinel', 'abeed', '$2b$10$E2b5fl4RcZy0zg2WHxHLbO', '$2b$10$E2b5fl4RcZy0zg2WHxHLbOVU.2GaO9KXzxbvHCH7dgiBXLR.YbF7m');
-INSERT INTO buses(number_plate, manufacturer, model, year, capacity) VALUES('H23652', 'Benz', 'Buccati', '2019', 7);
-INSERT INTO trips(id, bus_id, origin, destination, trip_date, fare ) VALUES (nextval('trips_sequence'), 'Lagos', 'Abuja', '2019-07-29', 20050.89);
-INSERT INTO bookings(trip_id, user_id, seat_number, created_on) VALUES(1, 1, 1, '2019-07-29');
+CREATE SEQUENCE generic_sequence START 1 INCREMENT 1;
+INSERT INTO users(user_id, is_admin, email, first_name, last_name, salt, hash ) VALUES(nextval('generic_sequence'), true, 'zabeed@outlook.com', 'zinel', 'abeed', '$2b$10$E2b5fl4RcZy0zg2WHxHLbO', '$2b$10$E2b5fl4RcZy0zg2WHxHLbOVU.2GaO9KXzxbvHCH7dgiBXLR.YbF7m');
+INSERT INTO buses(bus_id, number_plate, manufacturer, model, year, capacity) VALUES(nextval('generic_sequence'), 'H23652', 'Benz', 'Buccati', '2019', 7);
+INSERT INTO trips(id, bus_id, origin, destination, trip_date, fare ) VALUES(nextval('generic_sequence'), 1, 'Lagos', 'Abuja', '2019-07-29', 20050.89);
+INSERT INTO bookings(id, trip_id, user_id, seat_number, created_on) VALUES(nextval('generic_sequence'),1, 1, 1, '2019-07-29');
 COMMIT;
